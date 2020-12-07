223af50a62b6371dfb46ec60f0cf9037cde453c2
Chirag Bapat <bapatchirag@gmail.com>
2020-12-07 01:10:02 -0500

    Chat server and client

---
+++ exp5/chatclient.c
@@ -0,0 +1,81 @@
+#include "../socketheaders.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void chat_with_server(int);
+
+int main(int argc, char* argv[]) {
+    struct sockaddr_in client;
+    int socket_created;
+
+    // Missing address
+    if(argc == 1) {
+        printf("Missing address\n");
+        exit(0);
+    }
+
+    // Create client socket
+    if((socket_created = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) != -1) {
+        printf("Client socket created successfully\n");
+    }
+    else {
+        printf("Client socket failed to be created\n");
+        exit(0);
+    }
+
+    // Set port PORTNUM to connect to
+    client.sin_family = AF_INET;
+    client.sin_port = htons(PORTNUM);
+    inet_pton(AF_INET, argv[1], &client.sin_addr);
+
+    // Connect to server
+    if(!connect(socket_created, (struct sockaddr*)&client, sizeof(client))) {
+        printf("Client connected to server\n");
+    }
+    else {
+        printf("Couldn't connect to server\n");
+        exit(0);
+    }
+
+    // Chat with server
+    printf("-------------------------------------------------\n");
+    chat_with_server(socket_created);
+    
+    if(!close(socket_created)) {
+        printf("Connection closed successfully\n");
+    }
+    else {
+        printf("Connection couldn't be closed\n");
+        exit(0);
+    }
+
+    return 0;
+}
+
+void chat_with_server(int socket) {
+    int bytes_recv, to_cont;
+    char *buffer_send = malloc(1024);
+    char *buffer_recv = malloc(1024);
+
+    // Prompt
+    printf("Tell server: ");
+
+    while(strcmp(fgets(buffer_send, 1024, stdin), "end")) {
+        memset(buffer_recv, 0, 1024);
+        send(socket, buffer_send, strlen(buffer_send), 0);
+        memset(buffer_send, 0, 1024);
+        
+        // Receive response from server
+        if((to_cont = recv(socket, buffer_recv, 1024, 0)) > 0) {
+            printf("Server replies: %s\n", buffer_recv);
+        }
+
+        // Reprompt
+        printf("Tell server: ");
+    }
+
+    // Free memory
+    free(buffer_recv);
+    free(buffer_send);
+}
---
+++ exp5/chatserver.c
@@ -0,0 +1,98 @@
+#include "../socketheaders.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void chat_with_client(int);
+
+int main() {
+    struct sockaddr_in server;
+    int listenfd, connfd;
+
+    // Create socket
+    if((listenfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) != -1) {
+        printf("Server socket creation successful\n");
+    }
+    else {
+        printf("Server socket creation failed\n");
+        exit(0);
+    }
+
+    // Bind socket to PORTNUM
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(PORTNUM);
+
+    if(!bind(listenfd, (struct sockaddr*)&server, sizeof(server))) {
+        printf("Server socket bound to port %d\n", PORTNUM);
+    }
+    else {
+        printf("Server socket bind failed\n");
+        exit(0);
+    }
+
+    // Begin listening on socket
+    if(!listen(listenfd, MAXQUEUE)) {
+        printf("Server socket listening\n");
+    }
+    else {
+        printf("Server socket listen failed\n");
+    }
+
+    // Start accepting client requests
+    while(1) {
+        int server_len = sizeof(server);
+        if((connfd = accept(listenfd, (struct sockaddr*)&server, &server_len)) != -1) {
+            printf("Client connected to server\n");
+        }
+        else {
+            printf("Client request couldn't be accepted\n");
+            exit(0);
+        }
+
+        // Set up iterative server
+        // Close listening socket
+        if(!close(listenfd)) {
+            printf("Listening socket closed\n");
+        }
+        else {
+            printf("Listening socket close failed\n");
+            exit(0);
+        }
+
+        // Chat with client
+        chat_with_client(connfd);
+
+        // Close connected socket
+        if(!close(connfd)) {
+            printf("Connected socket closed\n");
+        }
+        else {
+            printf("Connected socket close failed\n");
+            exit(0);
+        }
+    }
+
+    return 0;
+}
+
+void chat_with_client(int socket) {
+    int bytes_recv;
+    char *buffer_recv = malloc(1024);
+    char *buffer_sent = malloc(1024);
+
+    do {
+        while((bytes_recv = recv(socket, buffer_recv, 1024, 0)) > 0) {
+            memset(buffer_sent, 0, 1024);
+            printf("Client says: %s\n", buffer_recv);
+            printf("Reply: ");
+            fgets(buffer_sent, 1024, stdin);
+            send(socket, buffer_sent, strlen(buffer_sent), 0);
+            memset(buffer_recv, 0, 1024);
+        }
+    } while(bytes_recv < 0);
+
+    // Free memory
+    free(buffer_recv);
+    free(buffer_sent);
+}
